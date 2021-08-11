bb503c1413827f442241894128c5c8821c487ee0
Дмитрий Грицай <dgritsay@inbox.ru>
2021-08-12 00:44:07 +0300

    Review 1.1 (models & fields corrected)

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(
         verbose_name='email', max_length=255, unique=True)
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
     USERNAME_FIELD = 'username'
 
     def __str__(self):
