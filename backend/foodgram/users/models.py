7a43e139f5cb27f6975938f70375a01bcb0c53cb
Дмитрий Грицай <dgritsay@inbox.ru>
2021-08-14 22:08:58 +0300

    Review 2.0 - requirements.txt corrected, gunicorn working

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class CustomUser(AbstractUser):
     email = models.EmailField(
         verbose_name='email', max_length=255, unique=True)
