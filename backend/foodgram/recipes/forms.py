bbbc6f3cd572a5985021966cd4235fb90f28c728
Дмитрий Грицай <dgritsay@inbox.ru>
2021-09-12 00:13:28 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	backend/foodgram/api/serializers.py

--- backend/foodgram/recipes/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from django import forms
-from .models import Recipe
-
-class RecipeForm(ModelForm):
-    class Meta:
-        model=Recipe
-        fields = ['tags', 'author',  'name', 'image', 'text', 'cooking_time' ]
